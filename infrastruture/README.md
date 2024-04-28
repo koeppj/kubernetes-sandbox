ded17ff47c6daecb0e37de1ac3743fe713c551ce
John Koepp <koeppj@koeppster.net>
2024-04-28 16:32:31 +0000

    Updates and added ClusterIssuer

--- infrastruture/README.md
+++ infrastruture/README.md
@@ -1,5 +1,7 @@
 # Infrastructure Namespace Setups
 
+## Overview
+
 ## Instructions
 
 To create run the following script.  Assumes aws cli configuration file present
@@ -13,6 +15,9 @@ and AWS Default Region+
 ## Components
 
 The following files comprise the items used to create the `infrastructure` namespace and resoruces contained therein.
-- `create-infrastructure.sh` - The shell script to run.
-- `Dockerfile` - Used to create the docker image that is used to generate and AWS ECR Docker Login token.
-- `aws-ecr-role-and-cron.yaml` A Kube file object that defines the a service account, RBAC role and cronjob used to periodically update AWS ECR Docker Login ticket and attach it to the `default` service account of the configured `namespace`s
+- [`create-infrastructure.sh`](./create-infrastructure.sh) - The shell script to run.
+- [`Dockerfile`](./Dockerfile) - Used to create the docker image that is used to generate an AWS ECR Docker Login token.
+- [`aws-ecr-role-and-cron.yaml`](./aws-ecr-role-and-cron.yaml) - A K8S template that defines the a service account, RBAC role and cronjob used to periodically update AWS ECR Docker Login ticket and attach it to the `default` service account of the configured `namespace`s
+- [`create-aws-credentials.yaml`](./create-aws-credentials.yaml) - A K8S template that defines defines the AWS Credentials `secret` in both the `infrastructure` and `default` namespaces (`AWS_ACCESS_KEY_ID`, `AWS_SECRET_ACCESS_KEY` and `AWS_DEFAULT_REGION`).
+- `create-cert-issuer.yaml` - A K83 template to create a LetsEncrypt based `CusterIssuer` using [cert-manager](https://cert-manager.io/)
+- [`create-storage-class.yaml`](./create-storage-class.yaml) - A K8S template to create a `StorageClass` based on the [NFS CSI](https://github.com/kubernetes-csi/csi-driver-nfs) storage driver.  Based on [this](https://microk8s.io/docs/how-to-nfs) how-to.
--- infrastruture/create-infrastructure.sh
+++ infrastruture/create-infrastructure.sh
@@ -9,6 +9,23 @@ if [ -f ~/.bash_aliases ]; then
     source ~/.bash_aliases
 fi
 #
+# Enable plugins
+#
+microk8s enable community
+microk8s enable registry
+microk8s enable cert-manager
+microk8s enable istio
+#
+# Install the CSI Driver so we can use NFS Storage
+#
+microk8s helm3 repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+microk8s helm3 repo update
+microk8s helm3 install csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
+    --namespace kube-system \
+    --set kubeletDir=/var/snap/microk8s/common/var/lib/kubelet
+# Wait for it to be ready
+microk8s kubectl wait pod --selector app.kubernetes.io/name=csi-driver-nfs --for condition=ready --namespace kube-system
+#
 # Create the docker image used by teh aws ecr updater job
 #
 docker build -t localhost:32000/awsecr --push .
@@ -21,19 +38,14 @@ kind: Namespace
 metadata: 
     name: infrastructure
 EOF
-aws_access_key_id=$(sed -rn '/aws_access_key_id = /p' ~/.aws/credentials | cut -d '=' -f2 | tr -d '[:space:]' | base64)
-aws_secret_access_key=$(sed -rn '/aws_secret_access_key = /p' ~/.aws/credentials | cut -d '=' -f2 | tr -d '[:space:]' | base64)
-aws_default_region=$(sed -rn '/region = /p' ~/.aws/config | cut -d '=' -f2 | tr -d '[:space:]' | base64)
-cat <<EOF | kubectl apply -f - 
-apiVersion: v1
-kind: Secret
-metadata:
-    name: aws-credentials
-    namespace: infrastructure
-type: Opaque
-data:
-    AWS_ACCESS_KEY_ID: ${aws_access_key_id}
-    AWS_SECRET_ACCESS_KEY: ${aws_secret_access_key}
-    AWS_DEFAULT_REGION: ${aws_default_region}
-EOF
+export aws_access_key_id=$(sed -rn '/aws_access_key_id = /p' ~/.aws/credentials | cut -d '=' -f2 | tr -d '[:space:]')
+export aws_access_key_id_encoded=$(sed -rn '/aws_access_key_id = /p' ~/.aws/credentials | cut -d '=' -f2 | tr -d '[:space:]' | base64)
+export aws_secret_access_key=$(sed -rn '/aws_secret_access_key = /p' ~/.aws/credentials | cut -d '=' -f2 | tr -d '[:space:]')
+export aws_secret_access_key_encoded=$(sed -rn '/aws_secret_access_key = /p' ~/.aws/credentials | cut -d '=' -f2 | tr -d '[:space:]' | base64)
+export aws_default_region=$(sed -rn '/region = /p' ~/.aws/config | cut -d '=' -f2 | tr -d '[:space:]')
+export aws_default_region_encoded=$(sed -rn '/region = /p' ~/.aws/config | cut -d '=' -f2 | tr -d '[:space:]' | base64)
+env | grep aws
+envsubst < create-aws-credentials.yaml | kubectl apply -f -
 kubectl apply -f aws-ecr-role-and-cron.yaml
+kubectl apply -f create-storage-class.yaml
+envsubst < create-cert-issuer.yaml | kubectl apply -f -
