e8f88648e3ae64b8f196f8135007366b877313da
John Koepp <koeppj@koeppster.net>
2025-09-04 14:26:47 +0000

    Built out n8n deployment, service and httproute - working as expected

--- n8n/scripts/deploy-n8n.sh
+++ n8n/scripts/deploy-n8n.sh
@@ -23,3 +23,6 @@ envsubst < ${manifests_dir}/postgres-secret.yaml | microk8s kubectl apply -f -
 microk8s kubectl apply -f ${manifests_dir}/postgres-configmap.yaml 
 envsubst < ${manifests_dir}/postgres-statefulset.yaml | microk8s kubectl apply -f -
 envsubst < ${manifests_dir}/n8n-persistent-volume-claim.yaml | microk8s kubectl apply -f -
+microk8s kubectl apply -f ${manifests_dir}/n8n-deployment.yaml
+microk8s kubectl apply -f ${manifests_dir}/n8n-service.yaml
+microk8s kubectl apply -f ${manifests_dir}/n8n-gateway.yaml
